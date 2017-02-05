3d6165fee73e147f79f6143bc0c77cb3696f9c47
Lazar Dilov <ldilov@yahoo.com>
2017-02-05 10:09:30 +0200

    Modified Exams table

--- exams.php
+++ exams.php
@@ -1,19 +1,27 @@
+<?php
+require_once('functions.php');
+logged_in();
+?>
 <table class="tg">
 	<colgroup>
 		<col style="width: 8%">
 		<col style="width: 15%">
 		<col style="width: 15%">
+		<col style="width: 5%">
 		<col style="width: 10%">
+		<col style="width: 8%">
 		<col style="width: 18%">
-		<col style="width: 18%">
+		<col style="width: 5%">
 	</colgroup>
 	  <tr>
 		<th class="tg-3we0">ФАКУЛТЕТ</th>
 		<th class="tg-3we0">ПРЕДМЕТ</th>
-		<th class="tg-3we0">Специалност/Група</th>
+		<th class="tg-3we0">СПЕЦИАЛНОСТ</th>
 		<th class="tg-3we0">ЗАЛА</th>
 		<th class="tg-3we0">ДАТА</th>
+		<th class="tg-3we0">ЧАС</th>
 		<th class="tg-3we0">ПРЕПОДАВАТЕЛ</th>
+		<th class="tg-3we0">КРЕДИТИ</th>
 	  </tr>
 <?php 
 $iter = 0;
@@ -21,12 +29,12 @@ $class = ["tg-dfop", "tg-mvlx"];
 if($_SESSION['type'] == 0) {
 ?>
 	<?php 
-	$query = "SELECT courses.name as subject, exams.faculty as faculty, exams.room as room, exams.date as date, title, teachers.firstName as fname, teachers.lastName as lname, programs.name as program, exams.group as group FROM exams 
+	$query = "SELECT courses.name as subject, exams.faculty as faculty, exams.room, exams.date, EXTRACT(HOUR from exams.hour) as hours, EXTRACT(MINUTE from exams.hour) as minutes, title, teachers.firstName as fname, teachers.lastName as lname, programs.name as program, courses.credits FROM exams 
 														  JOIN teachers on exams.teacher_id = teachers.id
 														  JOIN courses on exams.course_id = courses.id
 														  JOIN participate on exams.course_id = participate.course_id
 														  JOIN programs on programs.id = exams.program_id
-														  WHERE completed = 0 AND student_id = " . $_SESSION['id'];
+														  WHERE completed = 0 AND exams.program_id = ".$_SESSION['program'] . " AND student_id = " . $_SESSION['id'];
 	if(!$result = $link->query($query)){
 		echo "<div class=\"alert alert-info fade in\"> Няма налични изпити </div>";
 		echo "</table>";
@@ -35,12 +43,14 @@ if($_SESSION['type'] == 0) {
 	while($row = $result->fetch_assoc()){
 	?>
 	  <tr>
-		<td class="<?php echo $class[$iter]; ?>"><? echo $row['faculty']; ?></td>
-		<td class="<?php echo $class[$iter]; ?>" style="text-align: left"><? echo $row['subject']; ?></td>
-		<td class="<?php echo $class[$iter]; ?>"><? echo "Група " . $row['group'] . " " . $row['program'];  ?></td>
-		<td class="<?php echo $class[$iter]; ?>"><? echo $row['room'];    ?></td>
-		<td class="<?php echo $class[$iter]; ?>"><? echo $row['date'];    ?></td>
-		<td class="<?php echo $class[$iter]; ?>"><? echo $row['title']." ".$row['fname']. " ".$row['lname']; ?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['faculty']; ?></td>
+		<td class="<?php echo $class[$iter]; ?>" style="text-align: left"><?php echo $row['subject']; ?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['program'];  ?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['room'];    ?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['date'];    ?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['hours'].":".($row['minutes'] == 0 ? "00": $row['minutes']);?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['title']." ".$row['fname']. " ".$row['lname']; ?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['credits']; ?> <img src="images/icons/medal.png" /></td>
 	  </tr>
 	<?php 
 	$iter = $iter == 1? 0 : 1 ;
@@ -49,7 +59,7 @@ if($_SESSION['type'] == 0) {
 
 <?php
  } else {
-	 $query = "SELECT courses.name as subject, exams.faculty as faculty, exams.room as room, exams.date as date, title, firstName, lastName, programs.name as program FROM exams
+	 $query = "SELECT courses.name as subject, exams.faculty, exams.room, exams.date, exams.hour, title, firstName, lastName, programs.name as program, courses.credits FROM exams
 																										JOIN teachers on exams.teacher_id = teachers.id
 																										JOIN courses on exams.course_id = courses.id
 																										JOIN programs on programs.id = exams.program_id
@@ -68,7 +78,9 @@ if($_SESSION['type'] == 0) {
 		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['program'];  ?></td>
 		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['room'];    ?></td>
 		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['date'];    ?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['hours'].":".($row['minutes'] == 0 ? "00": $row['minutes']);?></td>
 		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['title']." ".$row['firstName']. " ".$row['lastName']; ?></td>
+		<td class="<?php echo $class[$iter]; ?>"><?php echo $row['credits']; ?> <img src="images/icons/medal.png" /></td>
 	  </tr>
 <?php
 	$iter = $iter == 1? 0 : 1 ;
--- install/database.sql
+++ install/database.sql
@@ -6,15 +6,17 @@ CREATE TABLE IF NOT EXISTS `programs` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `students` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
+  `fnumber` int(11) NOT NULL AUTO_INCREMENT,
   `password` varchar(255) NOT NULL,
   `firstName` varchar(255) NOT NULL,
+  `middleName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `degree` tinyint(1) NOT NULL DEFAULT 0,
   `program_id` int(11),
   `join_date`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`fnumber`),
+  UNIQUE(firstName, middleName, lastName),
   FOREIGN KEY (program_id) REFERENCES programs(id) 
 	ON DELETE SET NULL 
 	ON UPDATE CASCADE
@@ -22,12 +24,13 @@ CREATE TABLE IF NOT EXISTS `students` (
 
 CREATE TABLE IF NOT EXISTS `teachers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `title` varchar(255),
   `join_date`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE(firstName, lastName),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
@@ -35,14 +38,19 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
   `teacher_id` int(11),
   `credits` int(11) NOT NULL,
   `hour` TIME NOT NULL,
   `auditorium` varchar(255) NOT NULL,
-  `faculty` varchar(255) NOT NULL DEFAULT '‘Ã»',
+  `faculty` varchar(255) NOT NULL DEFAULT '???',
+  `program_id` int(11),
   PRIMARY KEY (`id`),
   FOREIGN KEY (teacher_id) REFERENCES teachers(id) 
 	ON DELETE SET NULL 
+	ON UPDATE CASCADE,
+  FOREIGN KEY (program_id) REFERENCES programs(id)
+	ON DELETE CASCADE
 	ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
@@ -50,7 +58,8 @@ CREATE TABLE IF NOT EXISTS `participate` (
   `student_id` int(11) NOT NULL AUTO_INCREMENT,
   `course_id` int(11) NOT NULL,
   `completed` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
-  FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  `mark` FLOAT(3,2) default 0,
+  FOREIGN KEY (student_id) REFERENCES students(fnumber) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
@@ -61,4 +70,14 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `content` varchar(255) NOT NULL,
   `date_created`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `exams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` int(11),
+  `teacher_id` int(11),
+  `date`  TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (teacher_id) REFERENCES teachers(id) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
