423d93a94eb115ba180bbca30f59a783fd4e334e
Lazar Dilov <ldilov@yahoo.com>
2017-02-25 11:06:37 +0200

    Fixed bug where you can log in admin panel as normal user.

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@ if(isset($_GET['loginFailed']) && isset($_SESSION['loginFailed'])){
 	exit();
 }
 
-if(!isset($_SESSION['verify'])){
+if(!isset($_SESSION['verify']) || $_SESSION['verify'] != 2){
 	include('account_err.php');
 	exit();
 }
@@ -56,5 +56,5 @@ if(isset($_GET['p'])){
 	$tpl = "home.php";
 }
 
-include ('template.php');
+include ('template.php');*
 ?>
